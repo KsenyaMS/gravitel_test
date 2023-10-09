7dd51efb62547d276a8bf78c04ffd42f7a5676dd
KsenyaMS <futya121992@gmail.com>
2023-10-09 12:19:47 +0400

    Правка

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter, Routes, Route} from "react-router-dom";
+import {BrowserRouter, Routes, Route, Navigate} from "react-router-dom";
 import LoginPage from './LoginPage';
 import { ApolloProvider } from '@apollo/client';
 import { client } from './client';
@@ -13,6 +13,10 @@ function App() {
         <Routes>
             <Route path="/login" element={<LoginPage/>}/>
             <Route path="/dashboard" element={<DashboardPage/>}/>
+            <Route
+                path="*"
+                element={<Navigate to="/login" replace />}
+            />
         </Routes>
       </BrowserRouter>
     </ApolloProvider>
